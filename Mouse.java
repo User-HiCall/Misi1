d047bfc7dba279001cd1aa04fec7c66e1b552f88
Haiqal Zakky Indi Febriansyah <haiqalzakky@gmail.com>
2025-11-06 12:47:46 +0700

    mouse fix

--- Mouse.java
+++ Mouse.java
@@ -2,4 +2,8 @@ public class Mouse extends Peripheral {
     public Mouse (int price, String name) {
         super(price, name);
     }
+
+    public void run () {
+        System.out.println("Mouse run smoothly");
+    }
 }
