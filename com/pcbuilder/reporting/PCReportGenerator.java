4c5d9f48d4393d74cd355bf5d9e6d5dc93e7227a
!rez <dimasrezaa117@gmail.com>
2025-11-06 14:44:49 +0700

    report now

--- com/pcbuilder/reporting/PCReportGenerator.java
+++ com/pcbuilder/reporting/PCReportGenerator.java
@@ -1,6 +1,8 @@
 package com.pcbuilder.reporting;
 
 import com.pcbuilder.core.PC;
+import com.pcbuilder.core.Ssd;
+import com.pcbuilder.core.Storage;
 
 public class PCReportGenerator {
     public static void generateReport(PC pc) {
@@ -8,6 +10,14 @@ public class PCReportGenerator {
 
     }
     public static void main(String[] args) {
-        
+        Storage ssd= new Ssd(500, "seagate ssd");
+        PC pc=new PC(ssd);
+
+        pc.generateReport();
+        pc.useMouse(null);
+        pc.useMonitor(null);
+        pc.useKeyboard(null);
+        pc.calculateTotalPrice(null, null, null);
+        System.out.println(""+);
     }
 }
