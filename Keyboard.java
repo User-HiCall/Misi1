9fc24f4b51034c06552a285a5b06e39eeb467705
!rez <dimasrezaa117@gmail.com>
2025-11-06 12:47:42 +0700

    keyboard

--- Keyboard.java
+++ Keyboard.java
@@ -2,4 +2,7 @@ public class Keyboard extends Peripheral{
     public Keyboard(int price,String name) {
         super(price, name);
     }
+    public void run(){
+        System.out.println("tok..tok");
+    }
 }
